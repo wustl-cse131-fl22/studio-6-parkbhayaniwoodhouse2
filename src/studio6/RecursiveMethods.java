4c6ba301f44afd97fdc2626e1f6000b7dc72b31a
selina <selina@seoyoungs-air.dhcp.wustl.edu>
2022-10-27 13:42:49 -0500

    .

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,15 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+
+		// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
-		
+		}
+		else {
+			return 1.0/(Math.pow(2,n)) + geometricSum(n-1);
+		}
+
 	}
 
 	/**
@@ -27,13 +32,15 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
-	}
 
-	
+		if (p % q == 0) {
+			return q;
+		}
+		else {
+			return gcd (q, p%q);
+		}
+
+	}
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -41,12 +48,35 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
-	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+
+	public static int[] methodReverse (int[]array, int[]reversed, int index) {
+
+		if (array.length == 1) {
+			reversed[0] = array[0];
+			return reversed;
+		}
+		else {
+			int lastIndex = array.length - 1;
+			int mirrorIndex = lastIndex - index;
+			
+			if (index > mirrorIndex) {
+				return reversed;
+			}
+			else {
+				reversed[index] = array[mirrorIndex];
+				reversed[mirrorIndex] = array[index];
+				return methodReverse (array,reversed,index+1);
+			}
+		}
 		
 	}
+
+	public static int[] toReversed(int[] array) {
+		int [] reversed = new int [array.length];
+		return methodReverse(array,reversed,0);
+
+	}
+
 
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
@@ -59,8 +89,17 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
+		 if (radius <= radiusMinimumDrawingThreshold) {
+			 
+		 }
+		 else {
+			 StdDraw.circle(xCenter, yCenter, radius);
+			 circlesUponCircles(xCenter + radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			 circlesUponCircles(xCenter, yCenter + radius, radius/3.0, radiusMinimumDrawingThreshold);
+			 circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			 circlesUponCircles(xCenter, yCenter - radius, radius/3.0, radiusMinimumDrawingThreshold);
+		 }
 		
-		// FIXME
 	}
 
 }
